960e489e4ac6cbd71cb8cf6bffacb0da06cb1f1d
Etem Utku Oylum <utkuoylum@Etems-MacBook-Air.local>
2024-08-12 11:28:01 +0200

    socials

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Nav from "@/components/Nav";
+import TopLeftImg from "@/components/TopLeftImg";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <TopLeftImg />
         <Nav />
         {children}
         </body>
